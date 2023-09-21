0a65a4da42174a97880f7a4577655bb9ef5521a9
Esther Onchieku <estheronchieku@gmail.com>
2023-09-21 15:13:00 -0500

    Added @RequestMapping "hellogoodbye"

--- src/main/java/org/launchcode/hellospring/controller/GreetingsController.java
+++ src/main/java/org/launchcode/hellospring/controller/GreetingsController.java
@@ -1,8 +1,7 @@
 package org.launchcode.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingsController {
@@ -11,10 +10,25 @@ public class GreetingsController {
     public String hello(){
         return "Hello Esther!";
     }
+    //responds to get requests at "/goodbye"
     @GetMapping("goodbye")
     @ResponseBody
 
     public String goodbye(){
         return "Adios Esther!";
     }
+
+    @PostMapping("shout")
+    @ResponseBody
+
+    public String sayWhat() {
+        return "Shout it, Esther!";
+    }
+    @RequestMapping(value="hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hellogoodbye(){
+        return "Hello and GoodBye";
+
+    }
+
 }
